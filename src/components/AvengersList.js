1a10c4057207711e7f25b4f952fd5d1fdfa84dfb
Kira <kawoods826@gmail.com>
2019-10-19 11:09:38 -0700

    added home and avengers list components

---
+++ src/components/AvengersList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const AvengerList = () => {
+  return (
+    <header>
+      <h1>Avenger List</h1>
+    </header>
+  );
+};
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <header>
+      <h1>Home</h1>
+    </header>
+  );
+};
